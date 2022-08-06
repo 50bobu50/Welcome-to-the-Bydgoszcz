b63955025da8e8b21e4f92816f1dc6b4becbed08
bobu <108267178+50bobu50@users.noreply.github.com>
2022-08-06 15:48:22 +0200

    Camera,Escape

--- Scanes/Player/Player.cs
+++ Scanes/Player/Player.cs
@@ -5,19 +5,24 @@ public class Player : KinematicBody
 {
 	public int Speed = 14;
 	public int FallAcceleration = 75;
-	public float MouseSensitivity = 0.05f;
+	public float MouseSensitivity = 0.005f;
 	private Vector3 _velocity = Vector3.Zero;
 
 
 	private Camera _camera;
+	private float _rotationX = 0f;
+	private float _rotationY = 0f;
 	private Spatial _character;
 	public override void _Ready()
 	{
+		Input.MouseMode = Input.MouseModeEnum.Captured;
 		_character = GetNode<Spatial>("Pivot");
 		_camera = GetNode<Camera>("Position3D/Camera");
 	}
 	public override void _PhysicsProcess(float delta)
 	{
+		Transform = Transform.Orthonormalized();
+
 		Vector3 direction = Vector3.Zero;
 
 		if(Input.IsActionPressed("move_right"))
@@ -43,44 +48,45 @@ public class Player : KinematicBody
 		if (direction != Vector3.Zero)
 		{
 			direction = direction.Normalized();
-			_character.LookAt(Translation + direction, Vector3.Up);
 		}
 
+		//chodzenie względem kamery
 		direction = direction.Rotated(Vector3.Up, _camera.GlobalRotation.y).Normalized();
-		GD.Print(direction);
-		GD.Print(_camera.GlobalRotation.y);
+		
 		//Ground Velcity
 		_velocity.x = direction.x * Speed;
 		_velocity.z = direction.z * Speed;
+		
 		//Vertical velocity
 		_velocity.y -= FallAcceleration * delta;
+		
 		// Moving the character
 		_velocity = MoveAndSlide(_velocity,Vector3.Up);
 	}
 
 	public override void _Input(InputEvent @event)
-	{
+	{	
   		// Mouse in viewport coordinates.
 		if (@event is InputEventMouseMotion && Input.MouseMode == Input.MouseModeEnum.Captured)
 		{
-
 			InputEventMouseMotion mouseEvent = @event as InputEventMouseMotion;
 			
-			_character.RotateX(Mathf.Deg2Rad(mouseEvent.Relative.y * MouseSensitivity));
-			RotateY(Mathf.Deg2Rad(-mouseEvent.Relative.x * MouseSensitivity));
+			_rotationX += -mouseEvent.Relative.x * MouseSensitivity;
+			_rotationY += -mouseEvent.Relative.y * MouseSensitivity;
 
-			Vector3 cameraRot = _camera.RotationDegrees;
-			cameraRot.x = Mathf.Clamp(cameraRot.x, -70, 70);
-			_character.RotationDegrees = cameraRot;	
+			//tak 👍
+			Transform transform = Transform;
+			transform.basis = Basis.Identity;
+			Transform = transform;
+
+			RotateObjectLocal(Vector3.Up,_rotationX);
+			RotateObjectLocal(Vector3.Right,_rotationY);
 		}
-		else if (@event is InputEventMouseButton eventMouseButton) 
+		else if (@event is InputEventKey eventKey) 
 		{
-			if (eventMouseButton.Pressed == true) {
-				Input.MouseMode = Input.MouseModeEnum.Captured;
-			}
-			else
+			if (eventKey.Pressed && eventKey.Scancode == (int)KeyList.Escape)
 			{
-				Input.MouseMode = Input.MouseModeEnum.Visible;
+				GetTree().Quit();
 			}
 		}
 	}
