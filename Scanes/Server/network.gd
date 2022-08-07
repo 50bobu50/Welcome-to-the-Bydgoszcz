3d9b9377936b24680d99fb0afec7a3d510f929c4
Qlesuga <110699693+Qlesuga@users.noreply.github.com>
2022-08-07 14:21:51 +0200

    lobby

--- Scanes/Server/network.gd
+++ Scanes/Server/network.gd
@@ -8,14 +8,18 @@ var MAX_PLAYERS  = 5
 var server
 var client 
 var players = {}
-var info = {name = "name"}
+var info = {"name":"name", "ready":false}
 
 func _ready():
-	#debug setup
+	# warning-ignore:return_value_discarded
 	get_tree().connect("connected_to_server", self, "_connected_ok")
+	# warning-ignore:return_value_discarded
 	get_tree().connect("server_disconnected", self, "_server_disconnected")
+	# warning-ignore:return_value_discarded
 	get_tree().connect("connection_failed", self, "_connected_fail")
+	# warning-ignore:return_value_discarded
 	get_tree().connect("network_peer_connected", self ,"_player_connected")
+	# warning-ignore:return_value_discarded
 	get_tree().connect("network_peer_connected", self ,"_player_disconnected")
 
 func create_server():
@@ -25,8 +29,7 @@ func create_server():
 	get_tree().network_peer = server
 	players[1] = info
 	print(players)
-
-
+	update_lobby()
 
 func join_server():
 	print("JOINING SERVER")
@@ -38,31 +41,48 @@ func reset_networking_connection():
 	if get_tree().has_network_peer():
 		get_tree().network.peer = null
 
-#Debug function
 func _connected_ok():
 	print("Seuccesfully connected to the server")
+	players[get_tree().get_network_unique_id()] = info
 
 func _server_disconnected():
 	print("Disconnected from to the server")
 	reset_networking_connection()
 
 func _connected_fail():
-	print("connection to the server failed")
+	print("Connection to the server failed")
 	reset_networking_connection()
 #player
 func _player_connected(id):
-	print("PLAYER: %s" % id)
+	print("PLAYER: %s CONNECTED" % id)
 	rpc_id(id, "register_player", info)
-	rpc("test","test")
 	
 func _player_disconnected(id):
+	print("PLAYER: %s DISCONNECTED" % id)
 	players.erase(id)
+	rpc("update_lobby")
 
-remote func register_player(info):
+remote func register_player(player_info):
 	var id = get_tree().get_rpc_sender_id()
-	players[id] = info
+	players[id] = player_info
 	print(players)
-	
-remote func test(test):
-	print(test)
+	rpc("update_lobby")
 
+sync func update_lobby():
+	get_tree().call_group("Lobby","update_lobby",players)
+	
+
+remote func set_ready(status):
+	var id = get_tree().get_network_unique_id()
+	rpc("player_ready",id,status)
+
+sync func player_ready(id,status):
+	players[id]["ready"]=status
+	update_lobby()
+	check_ready()
+	
+remote func check_ready():
+	for i in players:
+		if(players[i]["ready"]==false):
+			return null
+	print("GAME STARED")
--- Scanes/Server/network_setup.gd
+++ Scanes/Server/network_setup.gd
@@ -9,12 +9,12 @@ func _on_IP_text_changed(new_text):
 	print(new_text)
 
 func _on_HOST_pressed():
-	Network.create_server()
 	$"../LOBBY".show()
+	Network.create_server()
 	hide()
 
 
 func _on_JOIN_pressed():
-	Network.join_server()
 	$"../LOBBY".show()
+	Network.join_server()
 	hide()
