c1423ce519f3445182c469df7a6707b44d3e4320
bobu <108267178+50bobu50@users.noreply.github.com>
2022-08-09 01:00:29 +0200

    Movement,Momentum,Jump

--- Scanes/Player/Player.cs
+++ Scanes/Player/Player.cs
@@ -3,117 +3,70 @@ using System;
 
 public class Player : KinematicBody
 {
-	public float Speed = 1f;
-	public int MaxSpeed = 5;
-	public float CounterMometum = 0.2f;
-	public int FallAcceleration = 75;
-	public float MouseSensitivity = 0.0025f;
-	private Vector3 _velocity = Vector3.Zero;
+	float speed = 10f;
+	float acc = 10f;
+	float gravity = 10f;
+	float mouseSensitivity = 0.1f;
+	float jump = 3f;
 
-
-	private Camera _camera;
-	private float _rotationX = 0f;
-	private float _rotationY = 0f;
-	private Spatial _character;
+	Vector3 direction = Vector3.Zero;
+	Vector3 velocity = Vector3.Zero;
+	Vector3 gravityforce = Vector3.Zero;
+	//
+	Camera camera;
+	Position3D head;
+	Spatial character;
 	public override void _Ready()
 	{
 		Input.MouseMode = Input.MouseModeEnum.Captured;
-		_character = GetNode<Spatial>("Pivot");
-		_camera = GetNode<Camera>("Position3D/Camera");
+		character = GetNode<Spatial>("Pivot");
+		head = GetNode<Position3D>("Position3D");
+		camera = GetNode<Camera>("Position3D/Camera");
 	}
+
 	public override void _PhysicsProcess(float delta)
 	{
-		Transform = Transform.Orthonormalized();
+		//Direction
+		direction = Vector3.Zero;
+		//	KURWAAA
+		float hrot = GlobalTransform.basis.GetEuler().y;
+		float forwardInput = Input.GetActionStrength("move_back") - Input.GetActionStrength("move_forward");
+		float rightInput = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+		direction = new Vector3(rightInput,0,forwardInput).Rotated(Vector3.Up, hrot).Normalized();
 
-		Vector3 direction = Vector3.Zero;
 
-		if(Input.IsActionPressed("move_right"))
-		{
-			direction.x += 1f;
-		}
-
-		if (Input.IsActionPressed("move_left"))
-		{
-			direction.x -= 1f;
-		}
-
-		if (Input.IsActionPressed("move_back"))
-		{
-			direction.z += 1f;
-		}
-
-		if (Input.IsActionPressed("move_forward"))
-		{
-			direction.z -= 1f;
-		}
-
-		//chodzenie względem kamery
-		direction = direction.Rotated(Vector3.Up, _camera.GlobalRotation.y).Normalized();
-		//Ground Velcity
-		_velocity.x += direction.x * Speed;
-		_velocity.z += direction.z * Speed;
 		
-		
-		//GD.Print(direction);
-		
-		_velocity.x = Mathf.Clamp(_velocity.x,-MaxSpeed,MaxSpeed);
-		_velocity.z = Mathf.Clamp(_velocity.z,-MaxSpeed,MaxSpeed);
-
-		//Add Counter momentum
-		if (direction.x == 0)
+		if (IsOnFloor())
 		{
-			if (_velocity.x > 0)
-			{
-				_velocity.x -= Mathf.Clamp(direction.x,0,CounterMometum);
-			}
-			else if (_velocity.x < 0)
-			{
-				_velocity.x += Mathf.Clamp(-direction.x,0,CounterMometum);
-			}
+			gravityforce = Vector3.Zero;
+		}
+		else
+		{
+			gravityforce += Vector3.Down * gravity * delta;
 		}
 
-		if (direction.z == 0)
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
 		{
-			if (_velocity.z > 0)
-			{
-				_velocity.z -= Mathf.Clamp(direction.z,0,CounterMometum);
-			}
-			else if (_velocity.z < 0)
-			{
-				_velocity.z += Mathf.Clamp(-direction.z,0,CounterMometum);
-			}
+			gravityforce = Vector3.Up * jump;
 		}
 
-		//Vertical velocity
-		GD.Print("velocity X:"+Mathf.Round(_velocity.x)+" velocity Z:"+Mathf.Round(_velocity.z));
-		_velocity.y -= FallAcceleration * delta;
-		
-		// Moving the characte
-		_velocity = MoveAndSlide(_velocity,Vector3.Up);
+		velocity = velocity.LinearInterpolate(direction * speed, acc * delta);
+		velocity = velocity + gravityforce;
+		MoveAndSlide(velocity,Vector3.Up);
 	}
 
 	public override void _Input(InputEvent @event)
-	{	
-  		// Mouse in viewport coordinates.
-		if (@event is InputEventMouseMotion && Input.MouseMode == Input.MouseModeEnum.Captured)
+	{
+		if (@event is InputEventMouseMotion mouseMotion)
 		{
-			InputEventMouseMotion mouseEvent = @event as InputEventMouseMotion;
-			
-			_rotationX += -mouseEvent.Relative.x * MouseSensitivity;
-			_rotationY += -mouseEvent.Relative.y * MouseSensitivity;
-			
-			//muj konwertor 👍 
-			//Ograniczenie osi Y dla kamery (-50,50 stopni)
-			float rotationYDeg = Mathf.Rad2Deg(_rotationY);
-			_rotationY = Mathf.Deg2Rad(Mathf.Clamp(rotationYDeg,-50,50));
 
-			//tak 👍
-			Transform transform = Transform;
-			transform.basis = Basis.Identity;
-			Transform = transform;
-			
-			RotateObjectLocal(Vector3.Up,_rotationX);
-			RotateObjectLocal(Vector3.Right,_rotationY);
+			RotateY(Mathf.Deg2Rad(-mouseMotion.Relative.x * mouseSensitivity));
+			head.RotateX(Mathf.Deg2Rad(-mouseMotion.Relative.y * mouseSensitivity));
+
+			//Limit head rotation
+			Vector3 rotDeg = head.RotationDegrees;
+			rotDeg.x = Mathf.Clamp(rotDeg.x, -80f, 80f);
+			head.RotationDegrees = rotDeg;
 		}
 		else if (@event is InputEventKey eventKey) 
 		{
