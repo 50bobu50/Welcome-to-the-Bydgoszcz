5df6bcdb1dca1effd417da29b20115b0493e7341
Qlesuga <110699693+Qlesuga@users.noreply.github.com>
2022-08-16 15:31:05 +0200

    Gate + PickUp info

--- Scanes/Main/Game.cs
+++ Scanes/Main/Game.cs
@@ -5,8 +5,10 @@ public class Game : Node
 {
 	Label Collected;
 	Label Max;
+	CanvasItem PickUpText;
 	public int PointsCollected=0;
 	private int PointsMax;
+	private AnimationPlayer Gate;
 	public override void _Ready()
 	{
 		PointsMax = GetNode("MetaHolder").GetChildCount();
@@ -14,10 +16,28 @@ public class Game : Node
 		Collected = (Label)GetNode("UI/Collected");
 		Collected.Text = PointsCollected.ToString();
 		Max.Text = PointsMax.ToString();
+		Gate = (AnimationPlayer)GetNode("Navigation/NavigationMeshInstance/Gate/AnimationPlayer");
+		PickUpText = (CanvasItem)GetNode("UI/PickUpText");
 	}
 	
 	public void ShowPoints()
 	{
 		Collected.Text = PointsCollected.ToString();
+		PickUpText.Visible = true;
+		(PickUpText.GetNode("Timer") as Timer).Start();
+		if(PointsCollected==PointsMax)
+		{
+			CollectedEveryPoint();
+		}
+	} 
+	private void CollectedEveryPoint()
+	{
+		Gate.Play("Cube001Action");
+		Gate.Play("CubeAction001");
+	}
+	public void _on_Timer_timeout()
+	{
+		PickUpText.Visible = false;
 	}
 }
+
